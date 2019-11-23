08adaaf8aa31d75b57b6788168400cd4bd2e4476
YooAn <35682205+An98@users.noreply.github.com>
2019-11-23 13:27:43 +0000

    Add files via upload

--- application/controller/index.php
+++ application/controller/index.php
@@ -1,9 +1,9 @@
 <?php
 //register page
-include('../model/register.php');
+//include('../model/register.php');
 
 //connect to DB
-$connect = mysqli_connect("localhost","root","bemyprettyboy","klearning", 3306);
+$connect = mysqli_connect("localhost","klearning","webproject","klearning", 3306);
 if(!$connect) die('Not connected : ' . mysqli_error());
 
 //get form from index.html
@@ -13,9 +13,14 @@ $password = $_POST['password'];
 //check login button or register or guest
 //login
 if(isset($_POST['login'])){
-    $select_query = "select count(*) from user where USERID = "+$id+" and PASSWORD = "+$password;
-    if(mysqli_query($connect, $select_query) > 0){
+    $sql = "select nickname from user where USERID = "+$id+" and PASSWORD = "+$password;
+    $result = mysqli_query($conn,$sql);
+    
+    if((int)(mysqli_num_rows($result)) > 0){
         //call the game page
+        while($row = mysqli_fetch_assoc()){
+            echo " " . $row["nickname"]. "Welcome". "<br>";
+        }
         echo "SUCCESS!";
     }
     else{
@@ -29,6 +34,6 @@ if(isset($_POST['login'])){
 
 //guest
 
-
+    
 ?>
 
