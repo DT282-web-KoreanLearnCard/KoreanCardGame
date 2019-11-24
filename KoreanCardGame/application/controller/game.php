0b0b35169f0a6635e8a87bee619f37bcd3f77c48
YooAn <35682205+An98@users.noreply.github.com>
2019-11-24 15:56:22 +0000

    Add files via upload

--- KoreanCardGame/application/controller/game.php
+++ KoreanCardGame/application/controller/game.php
@@ -103,7 +103,7 @@ function makeCard(){
 <head>
     <meta charset="utf-8"/>
     <title>Game</title>
-    <link type="text/css" rel="stylesheet" href="../../static/css/game.css" id="creative">
+    <link type="text/css" rel="stylesheet" href="../../../KoreanLearn/static/css/game.css" id="creative">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="../../static/css/nav.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
