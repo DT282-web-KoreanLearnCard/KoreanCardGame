cf103a276aa6dfc3750bd06cf4ef0a38536b4681
to-besomeone <evillion99@gmail.com>
2019-11-24 12:48:16 +0000

    change tmp.html --> home.html delete game_index.php

--- application/controller/game_index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    include '/KoreanLearn/application/model/dbConnect.php';
-    ?>
--- application/model/guestCheck.php
+++ application/model/guestCheck.php
@@ -7,7 +7,7 @@
         echo "<html><head><title>Denied</title></head><body><h3>You cannot Access to this Page</h3><h4>Please Login</h4><p>Back to Home...</p></body></html>";
         flush();
 //        sleep(3);
-        echo "<meta http-equiv='refresh' content='1;url=../view/tmp.html?'>";
-//        header("Location: ../view/tmp.html");
+        echo "<meta http-equiv='refresh' content='1;url=../view/home.html?'>";
+//        header("Location: ../view/home.html");
     }
 ?>
