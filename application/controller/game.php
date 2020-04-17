47bd76dad15f9bbd087ba1e7f88467c3e1937551
to-besomeone <evillion99@gmail.com>
2020-04-17 18:06:22 +0900

    controller php

--- application/controller/game.php
+++ application/controller/game.php
@@ -1,8 +1,8 @@
 <?php
+include '../model/dbConnect.php';
 session_start();
 $cardNum = $_POST['pattern'];
 $category = $_POST['category'];
-$connect = mysqli_connect("localhost","klearning","webproject","klearning", 3306);
 $maxNum = $cardNum * $cardNum;
 $word=array();
 $saveId=array();
@@ -13,8 +13,8 @@ function insert(){
     global $connect,$cardNum,$category,$id;
 
     if(!isset($_SESSION['id'])){
-        echo "You cannot save the data";
-        echo "You should log in to save it";
+        echo "You cannot save the data. ";
+        echo "You should log in to save it.";
     }
     else{
         $sql="insert into gamelog(userid,score,categoryid) values('".$_SESSION['id']."',".$cardNum.",".$category.")";
@@ -83,6 +83,7 @@ function makeCard(){
     global $maxNum,$cardNum,$word,$saveIds,$saveId;
     $cnt=0;
     $arr = random(0,$maxNum-1,$maxNum);
+
     for($i=0; $i<$cardNum; $i++){
         echo "<div class='bord'>";
         for($j=0;$j<$cardNum;$j++){
@@ -95,22 +96,37 @@ function makeCard(){
         $saveIds = json_encode($saveId);
         echo "</div>";
     }
+
 }
 ?>
 <html>
 <head>
     <meta charset="utf-8"/>
     <title>Game</title>
-    <link type="text/css" rel="stylesheet" href="../../static/game.css" id="creative">
-
+    <link type="text/css" rel="stylesheet" href="../../static/css/game.css" id="creative">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="../../static/css/nav.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
+<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner" id="header"></header>
+<script>
+    $(function(){
+        $('#header').load('../../static/navbar.html');
+    })
+</script>
 
-<?php
-loadword();
-makeCard();
-?>
-
+<div class="main">
+    <div class="signup-content">
+        <?php
+            loadword();
+            makecard();
+        ?>
+        <div class = "animate" style = "background:rgb(81, 238, 162);height:0px; width:0px; display:inline-block;text-align:center;margin-left: 2px;border-radius: 50%; position: fixed; top: 30%; left: 30%;"></div>
+    </div>
+</div>
 <script>
     var click = ['',''];
     var indexs = [-1,-1];
@@ -125,15 +141,15 @@ makeCard();
     function change(id,index){
         if(click[0]==''){
             click[0]=id;
-            document.getElementById(click[0]).style.backgroundColor='hotpink';
+            document.getElementById(click[0]).style.backgroundColor='grey';
             indexs[0]=index;
         }
         else{
             var firstindex = saveIds[indexs[0]];
                 if(firstindex%2==0){
                     if(saveIds[index]==firstindex+1){
-                        document.getElementById(id).style.backgroundColor='black';
-                        document.getElementById(click[0]).style.backgroundColor='black';
+                        document.getElementById(id).style.backgroundColor='white';
+                        document.getElementById(click[0]).style.backgroundColor='white';
                         count++;
                         click[0]='';
                         indexs[0]=-1;
@@ -141,20 +157,33 @@ makeCard();
                 }
                 else{
                     if(saveIds[index]==firstindex-1){
-                        document.getElementById(id).style.backgroundColor='black';
-                        document.getElementById(click[0]).style.backgroundColor='black';
+                        document.getElementById(id).style.backgroundColor='white';
+                        document.getElementById(click[0]).style.backgroundColor='white';
                         count++;
                         click[0]='';
                         indexs[0]=-1;
                     }
                 }
             if(click[0]!=''){
-                document.getElementById(click[0]).style.backgroundColor='deepskyblue';
+                document.getElementById(click[0]).style.backgroundColor='black';
                 click[0]='';
                 indexs[0]=-1;
             }
             if(count==(cardNum*cardNum)/2){
                 alert("Success! <?php echo insert() ?>");
+                var div = $(".animate");
+
+                div.animate({
+                    height: '300px',
+                    width: '300px'
+                }, 2000);
+
+                div.animate({
+                    height: '0px',
+                    width: '0px'
+                }, 200);
+
+                setTimeout(function(){location.replace("../view/card_index.html")}, 3000);
 
             }
         }
--- application/controller/index.php
+++ application/controller/index.php
@@ -1,39 +1,30 @@
 <?php
-//register page
-//include('../model/register.php');
+    session_start();
+    //connect to DB
+    include '../model/dbConnect.php';
+    header("Content-Type: application/json");
+    $id = mysqli_real_escape_string($connect, $_POST['id']);
+    $password = mysqli_real_escape_string($connect, $_POST['password']);
+    $count = 0;
+    //checking results to see if any account has that username and password
+    $query = "select nickname from user where userid = '$id' and password = '$password'";
+    $count = mysqli_num_rows(mysqli_query($connect, $query));
+    $nickname = mysqli_fetch_assoc(mysqli_query($connect, $query))['nickname'];
+    if ($count == 0) {
+        //if no results found
+        echo json_encode(array('res'=>'FAIL'));
+//        echo "no account found";
 
-//connect to DB
-$connect = mysqli_connect("localhost","klearning","webproject","klearning", 3306);
-if(!$connect) die('Not connected : ' . mysqli_error());
-
-//get form from index.html
-$id = $_POST['id'];
-$password = $_POST['password'];
-
-//check login button or register or guest
-//login
-if(isset($_POST['login'])){
-    $sql = "select nickname from user where USERID = "+$id+" and PASSWORD = "+$password;
-    $result = mysqli_query($conn,$sql);
-    
-    if((int)(mysqli_num_rows($result)) > 0){
-        //call the game page
-        while($row = mysqli_fetch_assoc()){
-            echo " " . $row["nickname"]. "Welcome". "<br>";
-        }
-        echo "SUCCESS!";
-    }
-    else{
-        //login denied
-        echo "DENIED";
-    }
+    } else {
+        //set username to session variable
+        $_SESSION['id'] = $_POST['id'];
+        $_SESSION['password'] = $_POST['password'];
+        $_SESSION['nickname'] = $nickname;
+//        echo 'OK';
+        echo json_encode(array('res'=>'OK'));
+//        echo "welcome  " . $id;
+        //direct user to home page while being logged in
+//        header("Location:../view/card_index.php");
 }
-
-//register
-
-
-//guest
-
-    
 ?>
 
